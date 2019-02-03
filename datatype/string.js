f550bf78bd7ad27847d949fae2d601d83dc48571
cuppar <cuppar.hzy@gmail.com>
2019-02-03 13:00:27 +0800

    complete string part

---
+++ datatype/string.js
@@ -0,0 +1,13 @@
+// var s = '\u00A9';
+// console.log(s);
+// var f\u006F\u006f = 'abc';
+// console.log(foo);
+
+// var str = 'hello world!';
+// var base64_str = btoa(str);
+// var decode_str = atob(base64_str);
+// console.log(base64_str);
+// console.log(decode_str);
+
+var encode = encodeURIComponent('你好');
+console.log(encode);
